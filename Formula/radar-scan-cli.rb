405dfebb19cdb63699ca199f93761377746c2ca1
Raymon Epping <your@email.com>
2025-07-25 10:47:10 +0200

    🔖 radar-scan-cli: release v1.0.2

--- Formula/radar-scan-cli.rb
+++ Formula/radar-scan-cli.rb
@@ -1,10 +1,10 @@
 class RadarScanCli < Formula
   desc "Fast local secret scanning with Vault Radar, Markdown reports, and CI/CD support"
   homepage "https://github.com/raymonepping/radar-scan-cli"
-  url "https://github.com/raymonepping/homebrew-radar-scan-cli/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "cf850dbe06a3a70f4edfd962078df333fa23d41ade443aef5866fde68ca147fc"
+  url "https://github.com/raymonepping/homebrew-radar-scan-cli/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "8b829a2dbd51683f52becf494b6056e178bdd3dfbc6952034a0fe6f30d51d77e"
   license "MIT"
-  version "1.0.1"
+  version "1.0.2"
 
   depends_on "bash"
   depends_on "jq"
